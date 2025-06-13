9ceffb792a5dd1ec8d48f9ff69b1a96d4f17a24e
grouazel <antoine.grouazel@ifremer.fr>
2025-06-13 16:55:34 +0000

    add stuff from cookiecutter

---
+++ src/s1swotcolocs/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for s1swotcolocs."""
+
+__author__ = """Antoine Grouazel"""
+__email__ = 'antoine.grouazel@ifremer.fr'
+__version__ = '2025.6.13'
---
+++ tests/test_s1swotcolocs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""Tests for `s1swotcolocs` package."""
+
+import pytest
+
+
+from s1swotcolocs import s1swotcolocs
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
