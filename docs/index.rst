f1e0e3ae8f7ad7d5468b6be1d4e9552ffd061e1f
grouazel <antoine.grouazel@ifremer.fr>
2025-06-16 09:33:25 +0000

    add API .rst and unit tests

--- docs/index.rst
+++ docs/index.rst
@@ -8,7 +8,7 @@ Welcome to s1swotcolocs's documentation!
    readme
    installation
    usage
-   modules
+   api
    contributing
    authors
    history
--- src/s1swotcolocs/__init__.py
+++ src/s1swotcolocs/__init__.py
@@ -2,4 +2,8 @@
 
 __author__ = """Antoine Grouazel"""
 __email__ = 'antoine.grouazel@ifremer.fr'
-__version__ = '2025.6.13'
+try:
+    from importlib import metadata
+except ImportError:  # for Python<3.8
+    import importlib_metadata as metadata
+__version__ = metadata.version("sarwaveifrproc")
